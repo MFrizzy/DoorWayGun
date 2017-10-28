634233be5d39285241a8ec08ed8a4ac5a3547793
MFrizzy <at.frizzy@gmail.com>
2017-10-28 11:33:03 +0200

    début style

--- view/product/list.php
+++ view/product/list.php
@@ -7,5 +7,20 @@
  */
 
 foreach ($tab as $value){
-    echo '<p>'.htmlspecialchars($value->getIdProduct()).' : '.htmlspecialchars($value->getProductName());
+    echo   '<div class="demo-card-square mdl-card mdl-shadow--2dp">
+                <div class="mdl-card__title mdl-card--expand">
+                    <h2 class="mdl-card__title-text">'.htmlspecialchars($value->getProductName()).'</h2>
+                </div>
+                <div class="mdl-card__supporting-text">
+                   '.htmlspecialchars($value->getDescription()).'
+                </div>
+                <div class="mdl-card__actions mdl-card--border">
+                    <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+                        Acheter
+                    </a>
+                </div>
+            </div>';
+
 }
+
+
--- view/view.php
+++ view/view.php
@@ -3,23 +3,57 @@
 <head>
     <meta charset="UTF-8">
     <title><?php echo $pagetitle; ?></title>
-    <link rel="stylesheet" href="<?php File::build_path(array('style','material.min.css')) ?>">
+    <link rel="stylesheet" href="./style/material.min.css">
+    <script src="./style/material.min.js"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="./style/styles.css">
 </head>
 <body>
-<header>
-    <nav style="border: 1px solid black;padding-right:1em;">
-    </nav>
-</header>
-<?php
-$filepath = File::build_path(array("view", static::$object, "$view.php"));
-require $filepath;
-?>
 
-<footer>
-    <p style="border: 1px solid black;text-align:right;padding-right:1em;">
-        Footer
-    </p>
-</footer>
+<!-- Always shows a header, even in smaller screens. -->
+<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+        <div class="mdl-layout__header-row">
+            <!-- Title -->
+            <span class="mdl-layout-title">Doorway Gun</span>
+            <!-- Add spacer, to align navigation to the right -->
+            <div class="mdl-layout-spacer"></div>
+            <!-- Navigation. We hide it in small screens. -->
+            <nav class="mdl-navigation mdl-layout--large-screen-only">
+                <a class="mdl-navigation__link" href="./index.php">Products</a>
+                <a class="mdl-navigation__link" href="">Mon Compte</a>
+                <a class="mdl-navigation__link" href="">WLH</a>
+            </nav>
+        </div>
+    </header>
+    <div class="mdl-layout__drawer">
+        <span class="mdl-layout-title">Title</span>
+        <nav class="mdl-navigation">
+            <a class="mdl-navigation__link" href="./index.php">Products</a>
+            <a class="mdl-navigation__link" href="">Mon Compte</a>
+            <a class="mdl-navigation__link" href="">WLH</a>
+        </nav>
+    </div>
+    <main class="mdl-layout__content">
+        <div class="page-content">
+            <?php
+            $filepath = File::build_path(array("view", static::$object, "$view.php"));
+            require $filepath;
+            ?>
+        </div>
+    </main>
+
+    <footer class="mdl-mini-footer">
+        <div class="mdl-mini-footer__left-section">
+            <div class="mdl-logo">Title</div>
+            <ul class="mdl-mini-footer__link-list">
+                <li><a href="#">Help</a></li>
+                <li><a href="#">Privacy & Terms</a></li>
+            </ul>
+        </div>
+    </footer>
+
+</div>
 
 </body>
 </html>
