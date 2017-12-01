1ebabbdea5fa74f62f0ad2282582d4d74cb0704d
Unknown <weslie.rabeson@gmail.com>
2017-12-01 10:21:37 +0100

    we're having fun

--- view/user/confirm.php
+++ view/user/confirm.php
@@ -8,11 +8,12 @@
 
 ?>
 
-<div>
+<div class="mdl-card__supporting-text">
     <p>Êtes-vous sûr de vouloir nous quitter?</p>
     <p>
     <?php echo
-    '<a href="index.php?controller=user&action=deleted&idUser='.$user->getIdUser().'"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">Confirmer</button></a>';
+    '<a href="index.php?controller=user&action=deleted&idUser='.$user->getIdUser().'"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">Confirmer</button></a>
+    <a href="index.php?controller=user&action=readAll"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">Annuler</button></a>';
     ?>
     </p>
 </div>
