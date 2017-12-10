1e46fea05f1f1c6b227e5c564f3065a39c8ac3e4
Unknown <ducret.arnaud@gmail.com>
2017-12-10 17:18:35 +0100

    Ajout Du Button

--- view/product/list2.php
+++ view/product/list2.php
@@ -22,33 +22,43 @@
             </tr>
         </thead>
         <tbody>
-        <?php
-
-        foreach ($tab as $value) {
-            echo    '<tr>
-                        <th class="mdl-data-table__cell--non-numeric"><a href="index.php?controller=product&action=read&idProduct='.htmlspecialchars($value->getIdProduct()).'"><i class="material-icons">expand_more</i></a></th>
-                        <th>'.htmlspecialchars($value->getIdProduct()).'</th>
-                        <th class="mdl-data-table__cell--non-numeric">'.htmlspecialchars($value->getProductName()).'</th>
-                        <th>'.htmlspecialchars($value->getPrice()).'</th>
-                        <th class="mdl-data-table__cell--non-numeric"><a href="index.php?controller=product&action=update&idProduct='.htmlspecialchars($value->getIdProduct()).'"><i class="material-icons">mode_edit</i></a></th>
-                        <th class="mdl-data-table__cell--non-numeric"><a href="index.php?controller=product&action=updataImg&idProduct='.htmlspecialchars($value->getIdProduct()).'"><i class="material-icons">add_a_photo</i></a></th> 
-                        <th class="mdl-data-table__cell--non-numeric"><a href="index.php?controller=product&action=delete&idProduct='.htmlspecialchars($value->getIdProduct()).'"><i class="material-icons">delete</i></a></th>
+            <?php
+            foreach ($tab as $value) {
+                echo '<tr>
+                        <th class="mdl-data-table__cell--non-numeric"><a href="index.php?controller=product&action=read&idProduct=' . htmlspecialchars($value->getIdProduct()) . '"><i class="material-icons">expand_more</i></a></th>
+                        <th>' . htmlspecialchars($value->getIdProduct()) . '</th>
+                        <th class="mdl-data-table__cell--non-numeric">' . htmlspecialchars($value->getProductName()) . '</th>
+                        <th>' . htmlspecialchars($value->getPrice()) . '</th>
+                        <th class="mdl-data-table__cell--non-numeric"><a href="index.php?controller=product&action=update&idProduct=' . htmlspecialchars($value->getIdProduct()) . '"><i class="material-icons">mode_edit</i></a></th>
+                        <th class="mdl-data-table__cell--non-numeric"><a href="index.php?controller=product&action=updataImg&idProduct=' . htmlspecialchars($value->getIdProduct()) . '"><i class="material-icons">add_a_photo</i></a></th> 
+                        <th class="mdl-data-table__cell--non-numeric"><a href="index.php?controller=product&action=delete&idProduct=' . htmlspecialchars($value->getIdProduct()) . '"><i class="material-icons">delete</i></a></th>
                     </tr>
-            ';
-        }
+                ';
+            }
 
-        if($_GET['action']=='created') {
-            echo    '<div class="snackbar">
+            if ($_GET['action'] == 'created') {
+                echo '<div class="snackbar">
                         <div class="snackbar__text">Produit créé</div>
                     </div>';
-        }
-        elseif ($_GET['action']=='updated') {
-            echo    '<div class="snackbar">
+            } elseif ($_GET['action'] == 'updated') {
+                echo '<div class="snackbar">
                         <div class="snackbar__text">Produit modifié</div>
                     </div>';
-        }
+            }
+            ?>
 
+
+        </tbody>
+    </table>
+        <?php
+            echo ' 
+                <div class="list">
+                    <a href="index.php?controller=product&action=create">
+                        <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">
+                            Créer un produit
+                        </button>
+                    </a>
+                </div>
+            ';
         ?>
-</tbody>
-</table>
 </div>
