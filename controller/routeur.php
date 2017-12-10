68da8bfcc96208a97555c78153c1209b6d9e927f
Unknown <ducret.arnaud@gmail.com>
2017-12-10 16:22:43 +0100

    Default controller/action (réécriture presque complète de routeur)

--- controller/routeur.php
+++ controller/routeur.php
@@ -1,29 +1,38 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: tangu
  * Date: 28/10/2017
  * Time: 09:57
  */
+require_once File::build_path(array('controller', 'ControllerMain.php'));
+require_once File::build_path(array('controller', 'ControllerUser.php'));
+require_once File::build_path(array('controller', 'ControllerProduct.php'));
+require_once File::build_path(array('controller', 'ControllerBasket.php'));
+require_once File::build_path(array('controller', 'ControllerOrder.php'));
 
-require_once File::build_path(array('controller','ControllerMain.php'));
-require_once File::build_path(array('controller','ControllerUser.php'));
-require_once File::build_path(array('controller','ControllerProduct.php'));
-require_once File::build_path(array('controller','ControllerBasket.php'));
-require_once File::build_path(array('controller','ControllerOrder.php'));
+if (isset($_GET['controller'])) {
+    $controller_class = 'Controller' . ucfirst($_GET['controller']);
+} else {
+    $controller_class = 'ControllerProduct';
+}
 
-if(!isset($_GET['controller']) && !isset($_GET['action'])) ControllerProduct::readAll();
-else if(isset($_GET['controller']) && isset($_GET['action'])) {
-    $controller_class='Controller'.ucfirst($_GET['controller']);
-    if (class_exists($controller_class) && in_array($_GET['action'],get_class_methods($controller_class))) {
-        $action=$_GET['action'];
+if (isset($_GET['action'])) {
+    $action = $_GET['action'];
+} else {
+    $action = 'readAll';
+}
+
+if (class_exists($controller_class)) {
+    if (in_array($action, get_class_methods("$controller_class"))) {
         $controller_class::$action();
+    } else {
+        ControllerMain::erreur("Action inexistant");
     }
-    else {
-        ControllerMain::erreur("Controller ou action inexistant");
-    }
-
 }
 else {
-    ControllerMain::erreur("Il manque des informations");
+    $controller_class = 'ControllerProduct';
+    $action = 'readAll';
+    $controller_class::$action();
 }
