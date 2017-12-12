b0e51725659ffaf333d5e69029b1a5d727f750b3
Godzillax <ducret.arnaud@gmail.com>
2017-12-12 08:41:05 +0100

    Ajout bouton annuler suppr

--- view/user/confirm.php
+++ view/user/confirm.php
@@ -12,7 +12,8 @@
     <h5>Êtes-vous sûr de vouloir supprimer ce compte?</h5>
     <h5>
     <?php echo
-    '<a href="index.php?controller=user&action=deleted&idUser='.htmlspecialchars($user->getIdUser()).'"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">Confirmer</button></a>';
+    '<a href="index.php?controller=user&action=deleted&idUser='.htmlspecialchars($user->getIdUser()).'"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">Confirmer</button></a>
+    <a href="index.php?controller=user&action=readAll"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">Annuler</button></a>';
     ?>
     </h5>
 </div>
